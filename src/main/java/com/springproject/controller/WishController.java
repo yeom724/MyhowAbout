6b3f28c73dddb6b909283eee9792dccf10067680
YJY <yeam_3587@naver.com>
2024-12-26 18:01:21 +0900

    20241226

--- src/main/java/com/springproject/controller/WishController.java
+++ src/main/java/com/springproject/controller/WishController.java
@@ -1,5 +1,6 @@
 package com.springproject.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,31 +35,84 @@ public class WishController {
 	@Autowired
 	WishService wishService;
 	
+	private Member matchUserSession(HttpServletRequest req) {
+		
+		Member member = null;
+		HttpSession session = req.getSession(false);
+		
+		if(session != null) {
+			Member user = (Member)session.getAttribute("userStatus");
+			if(user != null) {
+				member = user;
+			}
+		}
+
+		return member;
+	}
+	
 	@ResponseBody
 	@PostMapping("/myPlace")
 	public Map<String, Boolean> addmyPlace(@RequestBody Map<String, String> map, HttpServletRequest req) {
 		
-		HttpSession session = req.getSession(false);
 		String placeID = map.get("placeID");
 		Place place = null;
 		Map<String, Boolean> data = new HashMap<String, Boolean>();
 		Boolean result = false;
 		
+		Member member = matchUserSession(req);
 		place = placeService.getPlace(placeID);
 		if(place != null) { placeService.addPlace(place); }
+
+		if(member != null) {
+			String userId = member.getUserId();
+			result = wishService.addWishList(userId, place);
+		}
+
+		data.put("result", result);
+		return data;
+	}
+	
+	@GetMapping("/mylist")
+	public String myWishList(HttpServletRequest req, Model model) {
 		
-		if(session != null) {
-			Member member = (Member)session.getAttribute("userStatus");
-			
-			if(member != null) {
-				 String userId = member.getUserId();
-				 result = wishService.addWishList(userId, place);
-			}
+		Member member = matchUserSession(req);
+		
+		if(member != null) {
+			String userId = member.getUserId();
+			List<Wish> myList = wishService.getMyList(userId);
+			model.addAttribute("list", myList);
+				
 		}
 		
-		data.put("result", result);
+		return "wishlist/wishList";
+	}
+	
+	@ResponseBody
+	@PostMapping("/delete/{placeID}/{userId}")
+	public String wishDelete(@PathVariable String placeID, @PathVariable String userId) {
+		wishService.deleteWish(placeID, userId);
+		return "true";
+	}
+	
+	
+	@GetMapping("/mapperMyList")
+	public String wishMapList(HttpServletRequest req, Model model) {
 		
-		return data;
+		Member member = matchUserSession(req);
+		if(member != null) {
+			String userId = member.getUserId();
+			List<Wish> myList = wishService.getMyList(userId);
+			ArrayList<Place> placelist = new ArrayList<Place>();
+			
+			for(int i=0; i<myList.size(); i++) {
+				Place place = placeService.getApiPlace(myList.get(i).getPlaceId());
+				placelist.add(place);
+			}
+			
+			model.addAttribute("list", placelist);
+		}
+		
+		return "wishlist/mapList";
 	}
 
 }
--- src/main/java/com/springproject/service/WishServiceImpl.java
+++ src/main/java/com/springproject/service/WishServiceImpl.java
@@ -1,9 +1,12 @@
 package com.springproject.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springproject.domain.Place;
+import com.springproject.domain.Wish;
 import com.springproject.repository.WishRepository;
 
 @Service
@@ -14,5 +17,11 @@ public class WishServiceImpl implements WishService{
 
 	@Override
 	public boolean addWishList(String userId, Place place) { return wishRepository.addWishList(userId, place); }
+
+	@Override
+	public List<Wish> getMyList(String userId) { return wishRepository.getMyList(userId); }
+
+	@Override
+	public void deleteWish(String placeID, String userId) { wishRepository.deleteWish(placeID, userId); }
 
 }
